71ae548d277e27a0334ff6300bc9ca42f02492d2
Gary Geng <garygengxiao@gmail.com>
2018-03-09 15:47:33 -0800

    Make code cleaner

--- extension/js/background.js
+++ extension/js/background.js
@@ -4,22 +4,54 @@ chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.action === 'login') {
       Promise.all([getAppInfo(request.appKey), getUserInfo(request.userKey)])
+        .catch(failToGetInfo)
         .then(matchForm)
-        .catch(failToGetInfo);
-      // chrome.tabs.create({ url: appInfo.url },
-      //   tab => handleNewTab($.extend({}, tab, matchedForm));
-      // });
+        .then(createNewTab)
+        .then((tabId, form) => startLogin(tabId, form));
     }
-  }
-);
+  });
 
 // TODO handle error
-function failToGetInfo() {
-  console.log('ERROR: Failed to get info.');
+function failToGetInfo(err) {
+  console.log(err);
 }
 
-// NOTE this looks very simple now because it is for testing. In production
-// this needs to make ajax call to our database
+// Matches the application form to the user form for convenience of filling later.
+function matchForm(dataArray) {
+  let appInfo = dataArray[0],
+    userInfo = dataArray[1];
+  // TODO merge them
+  return { little: "shit", url: 'https://apply.commonapp.org/login' };
+}
+
+function createNewTab(form) {
+  return new Promise(function (resolve, reject) {
+    chrome.tabs.create({ url: form.url },
+      tab => injectScripts(tab, form, resolve));
+  });
+}
+
+function injectScripts(tab, form, resolve) {
+  // TODO error handling
+  executeScripts(tab.id, [
+      { file: 'js/jquery.min.js' },
+      { file: 'js/injected.js' }
+    ],
+    tabId => resolve(tabId, form)
+  );
+}
+
+function startLogin(tabId, form) {
+  // TODO error handling
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabId, $.extend({}, { action: 'login' }, form));
+  });
+}
+
+/* NOTE this looks very simple now because it is for testing. In production
+ * this needs to make ajax call to our database. Additionally, it might be
+ * better to move getAppInfo and getUserInfo to injected.js
+ */
 function getAppInfo(appKey) {
   return $.getJSON('tests/app.json');
 }
@@ -27,27 +59,6 @@ function getAppInfo(appKey) {
 // NOTE same as getAppInfo
 function getUserInfo(userKey) {
   return $.getJSON('tests/user.json');
-}
-
-// Matches the application form to the user form for convenience of filling later.
-function matchForm(dataArray) {
-  let appInfo = dataArray[0];
-  let userInfo = dataArray[1];
-  // TODO merge them
-}
-
-function handleNewTab(tab) {
-  executeScripts(tab.id, [
-      { file: 'js/jquery.min.js' },
-      { file: 'js/content.js' }
-    ],
-    startLogin);
-}
-
-function startLogin(tabId) {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabId, { action: 'login' });
-  });
 }
 
 // Helper methods ===========================================================
--- extension/js/injected.js
+++ extension/js/injected.js
@@ -2,13 +2,8 @@
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    // $( window ).load(alert('login'));
     if (request.action === 'login') {
-      alert('boiii');
-      // $.getJSON('test/form.json', function (data) {
-      //   $('#ApplicantEmailAddress').val(data.username);
-      //   $('#ApplicantPassword').val(data.password);
-      // });
+      alert('Script injection successful.');
       $('#ApplicantEmailAddress').val("ggarland33@gmail.com");
       $('#ApplicantPassword').val("Somewhere-317");
     }
