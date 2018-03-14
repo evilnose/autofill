745bea3ea0128e2bf853111ad55b04280edc7f90
Gary Geng <garygengxiao@gmail.com>
2018-03-13 18:54:50 -0700

    rename files

---
+++ extension/js/loginInjection.js
@@ -0,0 +1,12 @@
+"use strict";
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.action === 'login') {
+      $(request.username).val(request.username_val);
+      $(request.password).val(request.password_val);
+      $(request.loginBtn).trigger('click');
+      // $('a[href="/ca4app#!c/0/11?sid=3"]').trigger('click');
+    }
+  }
+);
