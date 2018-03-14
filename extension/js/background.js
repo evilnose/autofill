581bbd7aa9dd6542b38fd0810fc224ff700dd559
Gary Geng <garygengxiao@gmail.com>
2018-03-13 18:53:30 -0700

    Be able to merge data and reorganize background.js for injection:

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,64 +1,131 @@
-'user strict';
+"use strict";
+
+var appInfo;
+var userInfo;
+var gForm;
+var loginInfo;
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.action === 'login') {
-      Promise.all([getAppInfo(request.appKey), getUserInfo(request.userKey)])
-        .catch(failToGetInfo)
-        .then(matchForm)
-        .then(createNewTab)
-        .then((tabId, form) => startLogin(tabId, form));
+      login(request.appKey, request.userKey, request.app_auth)
+        .then(startProcess);
     }
   });
+
+// Initial condition: appKey, userKey, and auth all exist
+function login(appKey, userKey, auth) {
+  loginInfo = auth;
+  return Promise.all([getAppInfo(appKey), getUserInfo(userKey)])
+    .catch(failToGetInfo)
+    .then(processForms)
+    .then(createNewTab)
+    .then(sendLogin)
+    .catch(failToLogin);
+}
+
+// Initial condition: user has logged in and is at the home url
+function startProcess() {
+
+}
 
 // TODO handle error
 function failToGetInfo(err) {
   console.log(err);
 }
 
-// Matches the application form to the user form for convenience of filling later.
-function matchForm(dataArray) {
-  let appInfo = dataArray[0],
-    userInfo = dataArray[1];
-  // TODO merge them
-  return { little: "shit", url: 'https://apply.commonapp.org/login' };
+// TODO handle error
+function failToLogin(err) {
+  console.log(err);
 }
 
-function createNewTab(form) {
+
+// Merge the app and user data into something easily parsable
+// Handle auth
+function processForms() {
+  // Duplicate app using shallow copying
+  gForm = $.extend({}, appInfo);
+
+  // Get necessary login values
+  // NOTE this is kind of tedious, but this allows data flattening
+  loginInfo.username = appInfo.username;
+  loginInfo.password = appInfo.password;
+  loginInfo.loginBtn = appInfo.loginBtn;
+
+  // Clear process array
+  gForm.process = [];
+  var oldPrc = appInfo.process;
+  var pLen = oldPrc.length;
+  var newI = 0;
+  let p;
+  let userVal;
+  let temp;
+  let key;
+  for (let i = 0; i < pLen; i++) {
+    p = oldPrc[i];
+    key = p.userkey;
+    delete p.userkey;
+    if (!p.action || p.action === 'f') {
+      userVal = user[key];
+      if (userVal) {
+        p.value = userVal;
+        gForm.process[newI++] = p;
+      } else {
+        console.log(`User does not have attribute ${key}`);
+      }
+    } else if (p.action === 'c' || p.action === 'r') {
+      gForm.process[newI++] = p;
+    }
+  }
+}
+
+function createNewTab() {
   return new Promise(function (resolve, reject) {
-    chrome.tabs.create({ url: form.url },
-      tab => injectScripts(tab, form, resolve));
+    // Use a virtual schema for url
+    chrome.tabs.create({ url: (gForm.origin + gForm.login) },
+      tab => injectLoginScripts(tab, resolve));
   });
 }
 
-function injectScripts(tab, form, resolve) {
+function injectLoginScripts(tab, resolve) {
   // TODO error handling
   executeScripts(tab.id, [
       { file: 'js/jquery.min.js' },
-      { file: 'js/injected.js' }
+      { file: 'js/loginInjection.js' }
     ],
-    tabId => resolve(tabId, form)
+    resolve
   );
 }
 
-function startLogin(tabId, form) {
+function sendLogin(tabId) {
   // TODO error handling
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabId, $.extend({}, { action: 'login' }, form));
+    chrome.tabs.sendMessage(tabId, $.extend({}, loginInfo, { action: 'login' }));
   });
 }
 
-/* NOTE this looks very simple now because it is for testing. In production
+/* TODO this looks very simple now because it is for testing. In production
  * this needs to make ajax call to our database. Additionally, it might be
- * better to move getAppInfo and getUserInfo to injected.js
+ * better to move getAppInfo and getUserInfo to .js
  */
 function getAppInfo(appKey) {
-  return $.getJSON('tests/app.json');
+  return new Promise(function (resolve, reject) {
+    $.getJSON('storage/app.json').then(function (data) {
+      appInfo = data[appKey];
+      resolve();
+    }, reject);
+  });
 }
 
 // NOTE same as getAppInfo
 function getUserInfo(userKey) {
-  return $.getJSON('tests/user.json');
+  // Of course we're not gonna get the entire user base in reality, ha, ha.
+  return new Promise(function (resolve, reject) {
+    $.getJSON('storage/user.json').then(function (data) {
+      userInfo = data[userKey];
+      resolve();
+    }, reject);
+  });
 }
 
 // Helper methods ===========================================================
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,7 +1,27 @@
 $(function () {
   $('#test').click(function () {
+    var userKey = 0;
+    var appName = 'commonapp';
     // NOTE appName is only a temporary measure. AppId will ensure that
     // the app is unique
-    chrome.runtime.sendMessage({ action: 'login', appKey: 'commonapp'});
+    getUserAuth(userKey, appName)
+      .then(auth => sendTestMessage(auth, userKey, appName));
   });
 });
+
+function getUserAuth(userKey, appName) {
+  // Of course we're not gonna get the entire user base in reality, ha, ha.
+  return new Promise(function (resolve, reject) {
+    $.getJSON('storage/user.json').then(
+      data => resolve(data[userKey][`auth_${appName}`]), reject);
+  });
+}
+
+function sendTestMessage(app_auth, userKey, appName) {
+  chrome.runtime.sendMessage({
+    action: 'login',
+    appKey: appName,
+    userKey: userKey,
+    app_auth
+  });
+}
