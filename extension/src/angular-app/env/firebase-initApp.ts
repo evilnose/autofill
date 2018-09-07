6122971440474cef2d0f9b3cdd9cb0f757e9c34e
Gary Geng <garygengxiao@gmail.com>
2018-09-07 11:39:00 -0500

    remove credentials

--- extension/src/angular-app/env/firebase-initApp.ts
+++
@@ -1,8 +0,0 @@
-export const initAppConfig = {
-    "apiKey": "AIzaSyCyczTiCl2w-rvUtKz-zj5VTayYF9QoANc",
-    "authDomain": "college-app-autofill.firebaseapp.com",
-    "databaseURL": "https://college-app-autofill.firebaseio.com",
-    "messagingSenderId": "109331497527",
-    "projectId": "college-app-autofill",
-    "storageBucket": "college-app-autofill.appspot.com"
-};
