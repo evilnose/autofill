23f93ce96d129e4bf8402f9c3e465487523ab3a4
Gary Geng <garygengxiao@gmail.com>
2018-09-06 23:57:51 -0500

    Move firebase credentials

---
+++ extension/src/angular-app/env/firebase-initApp.ts
@@ -0,0 +1,8 @@
+export const initAppConfig = {
+    "apiKey": "AIzaSyCyczTiCl2w-rvUtKz-zj5VTayYF9QoANc",
+    "authDomain": "college-app-autofill.firebaseapp.com",
+    "databaseURL": "https://college-app-autofill.firebaseio.com",
+    "messagingSenderId": "109331497527",
+    "projectId": "college-app-autofill",
+    "storageBucket": "college-app-autofill.appspot.com"
+};
